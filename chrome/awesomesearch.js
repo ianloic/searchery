86e1883a1262d0fbf238bedcfd078e6ff0610bb6
Ian McKellar <ian@mckellar.org>
2008-04-26 14:15:44 -0700

    find useful elements

--- chrome/awesomesearch.js
+++ chrome/awesomesearch.js
@@ -9,12 +9,19 @@ AwesomeSearch.consoleService =
 AwesomeSearch.log = function(aMessage) {
   aMessage = 'AwesomeSearch: ' + aMessage;
   this.consoleService.logStringMessage(aMessage);
-  dump(aMessage);
+  dump(aMessage+'\n');
 }
 
 // initialization
 AwesomeSearch.windowOnLoad = function() {
   this.log('windowOnLoad');
+
+  // find all the page elements we care about
+  this.iframe = document.getElementById('__AS_iframe');
+  this.urlbar = document.getElementById('urlbar');
+  this.popup = document.getElementById('PopupAutoCompleteRichResult');
+  this.richlistbox = document.getAnonymousElementByAttribute(this.popup,
+      'anonid', 'richlistbox');
 }
 
 
