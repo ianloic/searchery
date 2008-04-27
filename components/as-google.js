d88fe715d31e661f49e5ff8f969651b0389187ad
Ian McKellar <ian@mckellar.org>
2008-04-26 23:54:13 -0700

    when 2 become 1

--- components/as-google.js
+++ components/as-google.js
@@ -4,150 +4,125 @@ const Ci = Components.interfaces;
 const Cr = Components.results;
 const Cu = Components.utils;
 
+const GOOGLE_API_KEY = 'ABQIAAAAB5EMLNLcl9tsZ4hqbawAfxRs0Gcr6QwT8A5mowOxfafuDSDZ8xRyV6raPQ50DzlWqr9-QcqYSI5gOg';
+const GOOGLE_API_REFERRER = 'http://ianloic.com/';
+
 // import the XPCOM helper
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-
-// Implements nsIAutoCompleteResult
-function WebSearchAutoCompleteResult(searchString, searchResult,
-                                  defaultIndex, errorDescription,
-                                  results, comments) {
-  this._searchString = searchString;
-  this._searchResult = searchResult;
-  this._defaultIndex = defaultIndex;
-  this._errorDescription = errorDescription;
-  this._results = results;
-  this._comments = comments;
-  dump('WebSearchAutoCompleteResult()\n');
-}
-
-WebSearchAutoCompleteResult.prototype = {
-  _searchString: "",
-  _searchResult: 0,
-  _defaultIndex: 0,
-  _errorDescription: "",
-  _results: [],
-  _comments: [],
-
-  /**
-   * The original search string
-   */
-  get searchString() {
-    dump('get searchString: '+this._searchString+'\n');
-    return this._searchString;
-  },
-
-  /**
-   * The result code of this result object, either:
-   *         RESULT_IGNORED   (invalid searchString)
-   *         RESULT_FAILURE   (failure)
-   *         RESULT_NOMATCH   (no matches found)
-   *         RESULT_SUCCESS   (matches found)
-   */
-  get searchResult() {
-    dump('get searchResult: '+this._searchResult+'\n');
-    return this._searchResult
-  },
-
-  /**
-   * Index of the default item that should be entered if none is selected
-   */
-  get defaultIndex() {
-    dump('get defaultIndex: '+this._defaultIndex+'\n');
-    return this._defaultIndex;
-  },
-
-  /**
-   * A string describing the cause of a search failure
-   */
-  get errorDescription() {
-    dump('get errorDescription: '+this._errorDescription+'\n');
-    return this._errorDescription;
-  },
-
-  /**
-   * The number of matches
-   */
-  get matchCount() {
-    dump('get matchCount: '+this._results.length+'\n');
-    return this._results.length;
-  },
-
-  /**
-   * Get the value of the result at the given index
-   */
-  getValueAt: function(index) {
-    dump('getValueAt('+index+'): '+this._results[index]+'\n');
-    return this._results[index];
-  },
-
-  /**
-   * Get the comment of the result at the given index
-   */
-  getCommentAt: function(index) {
-    dump('getCommentAt('+index+'): '+this._comments[index]+'\n');
-    return this._comments[index];
-  },
-
-  /**
-   * Get the style hint for the result at the given index
-   */
-  getStyleAt: function(index) {
-    dump('getStyleAt('+index+')\n')
-    if (!this._comments[index])
-      return null;  // not a category label, so no special styling
-
-    if (index == 0)
-      return "suggestfirst";  // category label on first line of results
-
-    return "suggesthint";   // category label on any other line of results
-  },
-
-  getImageAt: function(index) {
-    dump('getImageAt('+index+')\n');
-  },
-
-  /**
-   * Remove the value at the given index from the autocomplete results.
-   * If removeFromDb is set to true, the value should be removed from
-   * persistent storage as well.
-   */
-  removeValueAt: function(index, removeFromDb) {
-    dump('removeValueAt\n');
-    this._results.splice(index, 1);
-    this._comments.splice(index, 1);
-  },
-
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAutoCompleteResult])
-};
+// import JSON utils
+Cu.import("resource://gre/modules/JSON.jsm");
 
 
 // Implements nsIAutoCompleteSearch
 function WebSearchAutoCompleteSearch() {
-  dump('WebSearchAutoCompleteSearch\n');
+  this._xhr = null;
 }
 
 WebSearchAutoCompleteSearch.prototype = {
   // start a search
   startSearch: function(searchString, searchParam, previousResult, listener) {
-    dump('startSearch: '+searchString+'\n');
-    var results = ['http://www.google.com/','http://ian.mckellar.org/'];
-    var comments = ['Google', 'Ian McKellar'];
-    var newResult = new WebSearchAutoCompleteResult('',
-        Ci.nsIAutoCompleteResult.RESULT_SUCCESS, 0, "", results, comments);
-    listener.onSearchResult(this, newResult);
-    dump('startSearch complete\n')
+    // set state
+    this.searchString = searchString;
+    this.defaultIndex = 0;
+    this.matchCount = 0;
+    this.searchResult = 0;
+    this._results = []
+
+    // save off the listener
+    this._listener = listener;
+
+    // create an XMLHttpRequest object to talk to the server
+    this._xhr = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance();
+
+    // add event handlers
+    this._xhr.QueryInterface(Ci.nsIDOMEventTarget);
+    var self = this;
+    this._xhr.addEventListener("load", function(evt) { self._onLoad(evt); },
+                               false);
+    this._xhr.addEventListener("error", function(evt) { self._onError(evt); },
+                               false);
+
+    // make the request
+    this._xhr.QueryInterface(Ci.nsIXMLHttpRequest);
+    this._xhr.open('GET',
+        'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' +
+        encodeURIComponent(searchString), true);
+    this._xhr.send(null);
   },
 
   // Stop at search
   stopSearch: function() {
-    dump('stopSearch\n')
+    if (this._xhr) {
+      this._xhr.abort();
+    }
+  },
+
+  _onLoad: function(event) {
+    var data;
+    try {
+      data = JSON.fromString(this._xhr.responseText);
+    } catch (e) {
+      dump('error: '+e+'\n');
+    }
+    this._results = [];
+    for (var i=0; i<data.responseData.results.length;i++) {
+      var result = data.responseData.results[i];
+      this._results.push({
+        value: result.unescapedUrl,
+        comment: result.titleNoFormatting,
+        image: 'http://www.google.com/favicon.ico',
+        style: (i?'suggesthint':'suggestfirst')
+      });
+    }
+    this.matchCount = this._results.length;
+    this.searchResult = Ci.nsIAutoCompleteResult.RESULT_SUCCESS;
+
+    this._listener.onSearchResult(this, this);
+    this._xhr = null;
+  },
+
+  _onError: function(event) {
+    dump('onError\n')
+  },
+
+  getCommentAt: function(index) {
+    if (!this._results || index >= this._results.length) {
+      return null;
+    }
+    return this._results[index].comment;
+  },
+
+  getImageAt: function(index) {
+    if (!this._results || index >= this._results.length) {
+      return null;
+    }
+    return this._results[index].image;
+  },
+
+  getStyleAt: function(index) {
+    if (!this._results || index >= this._results.length) {
+      return null;
+    }
+    return this._results[index].style;
+  },
+
+  getValueAt: function(index) {
+    if (!this._results || index >= this._results.length) {
+      return null;
+    }
+    return this._results[index].value;
+  },
+
+  removeValueAt: function(index, removeFromDb) {
+    // FIXME: implement? why?
   },
 
   // XPCOM registration
   classDescription: "AwesomeSearch Google AutoComplete",
   contractID: "@mozilla.org/autocomplete/search;1?name=as-google",
   classID: Components.ID("7ffb0fd2-b67d-48d8-b9d0-7069764cb448"),
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAutoCompleteSearch])
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAutoCompleteSearch,
+      Ci.nsIAutoCompleteResult])
 };
 
 function NSGetModule(compMgr, fileSpec) {
