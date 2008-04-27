50fd8d288acd8d06bea40ffb9f869a35ec2f2366
Ian McKellar <ian@mckellar.org>
2008-04-27 01:07:57 -0700

    genericalize

--- components/as-google.js
+++ components/as-google.js
@@ -41,11 +41,8 @@ BaseAutoCompleteSearch.prototype = {
                                false);
 
     // make the request
-    this._xhr.QueryInterface(Ci.nsIXMLHttpRequest);
-    this._xhr.open('GET',
-        'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' +
-        encodeURIComponent(searchString), true);
-    this._xhr.send(null);
+    this.makeRequest();
+
   },
 
   // Stop at search
@@ -56,33 +53,19 @@ BaseAutoCompleteSearch.prototype = {
   },
 
   _onLoad: function(event) {
-    var data;
-    try {
-      data = JSON.fromString(this._xhr.responseText);
-    } catch (e) {
-      // if we get an error here let's handle it with a smile
+    this._results = [];
+    if (this.handleResponse()) {
+      this.matchCount = this._results.length;
+      this.searchResult = Ci.nsIAutoCompleteResult.RESULT_SUCCESS;
+      this._listener.onSearchResult(this, this);
+      this._xhr = null;
+    } else {
       this.matchCount = 0;
       this.searchResult = Ci.nsIAutoCompleteResult.RESULT_FAILURE;
       this._results = [];
       this._listener.onSearchResult(this, this);
       this._xhr = null;
-      return;
     }
-    this._results = [];
-    for (var i=0; i<data.responseData.results.length;i++) {
-      var result = data.responseData.results[i];
-      this._results.push({
-        value: result.unescapedUrl,
-        comment: result.titleNoFormatting,
-        image: 'http://www.google.com/favicon.ico',
-        style: (i?'suggesthint':'suggestfirst') + ' awesomesearch as-google'
-      });
-    }
-    this.matchCount = this._results.length;
-    this.searchResult = Ci.nsIAutoCompleteResult.RESULT_SUCCESS;
-
-    this._listener.onSearchResult(this, this);
-    this._xhr = null;
   },
 
   _onError: function(event) {
@@ -138,6 +121,32 @@ GoogleAutoCompleteSearch.prototype.contractID =
     '@mozilla.org/autocomplete/search;1?name=as-google';
 GoogleAutoCompleteSearch.prototype.classID =
     Components.ID("7ffb0fd2-b67d-48d8-b9d0-7069764cb448");
+GoogleAutoCompleteSearch.prototype.makeRequest = function () {
+  this._xhr.QueryInterface(Ci.nsIXMLHttpRequest);
+  this._xhr.open('GET',
+      'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' +
+      encodeURIComponent(this.searchString), true);
+  this._xhr.send(null);
+}
+GoogleAutoCompleteSearch.prototype.handleResponse = function () {
+  var data;
+  try {
+    data = JSON.fromString(this._xhr.responseText);
+  } catch (e) {
+    // if we get an error here let's handle it with a smile
+    return false;
+  }
+  for (var i=0; i<data.responseData.results.length;i++) {
+    var result = data.responseData.results[i];
+    this._results.push({
+      value: result.unescapedUrl,
+      comment: result.titleNoFormatting,
+      image: 'http://www.google.com/favicon.ico',
+      style: (i?'suggesthint':'suggestfirst') + ' awesomesearch as-google'
+    });
+  }
+  return true;
+}
 
 function NSGetModule(compMgr, fileSpec) {
   return XPCOMUtils.generateModule([GoogleAutoCompleteSearch]);
