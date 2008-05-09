f1bd884c010f81cefcdb2bcb81aa236a9c4fdcae
Ian McKellar <ian@mckellar.org>
2008-05-09 15:00:53 -0700

    ensanitize

--- components/search-base.js
+++ components/search-base.js
@@ -10,84 +10,96 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 // Implements nsIAutoCompleteSearch
 function SearchBase() {}
 
-SearchBase.prototype = {
-  // start a search
-  startSearch: function(searchString, searchParam, previousResult, listener) {
-    // set state
-    this.searchString = searchString;
-    this.defaultIndex = 0;
+// start a search
+SearchBase.prototype.startSearch = 
+function SearchBase_startSearch(searchString, searchParam, previousResult, 
+    listener) {
+  // set state
+  this.searchString = searchString;
+  this.defaultIndex = 0;
+  this.matchCount = 0;
+  this.searchResult = 0;
+  this._results = []
+
+  // save off the listener
+  this._listener = listener;
+}
+
+// Stop at search
+SearchBase.prototype.stopSearch = 
+function SearchBase_stopSearch() {
+}
+
+SearchBase.prototype.getCommentAt =
+function SearchBase_getCommentAt(index) {
+  if (!this._results || index >= this._results.length) {
+    return null;
+  }
+  return this._results[index].comment;
+}
+
+SearchBase.prototype.getImageAt =
+function SearchBase_getImageAt(index) {
+  if (!this._results || index >= this._results.length) {
+    return null;
+  }
+  return this._results[index].image;
+}
+
+SearchBase.prototype.getStyleAt =
+function SearchBase_getStyleAt(index) {
+  if (!this._results || index >= this._results.length) {
+    return null;
+  }
+  return this._results[index].style;
+}
+
+SearchBase.prototype.getValueAt =
+function SearchBase_getValueAt(index) {
+  if (!this._results || index >= this._results.length) {
+    return null;
+  }
+  return this._results[index].value;
+},
+
+SearchBase.prototype.removeValueAt =
+function SearchBase_removeValueAt(index, removeFromDb) {
+}
+
+SearchBase.prototype.clearResults =
+function SearchBase_clearResults() {
+  this._results = [];
+},
+
+SearchBase.prototype.addResult =
+function SearchBase_addResult(aValue, aComment, aImage, aStyle) {
+  this._results.push({
+    value: aValue,
+    comment: aComment,
+    image: aImage,
+    style: aStyle
+  });
+},
+
+SearchBase.prototype.notifyListener =
+function SearchBase_notifyListener(aSucceeded) {
+  if (aSucceeded) {
+    this.matchCount = this._results.length;
+    this.searchResult = Ci.nsIAutoCompleteResult.RESULT_SUCCESS;
+    this._listener.onSearchResult(this, this);
+  } else {
     this.matchCount = 0;
-    this.searchResult = 0;
-    this._results = []
-
-    // save off the listener
-    this._listener = listener;
-  },
-
-  // Stop at search
-  stopSearch: function() {
-  },
-
-  getCommentAt: function(index) {
-    if (!this._results || index >= this._results.length) {
-      return null;
-    }
-    return this._results[index].comment;
-  },
-
-  getImageAt: function(index) {
-    if (!this._results || index >= this._results.length) {
-      return null;
-    }
-    return this._results[index].image;
-  },
-
-  getStyleAt: function(index) {
-    if (!this._results || index >= this._results.length) {
-      return null;
-    }
-    return this._results[index].style;
-  },
-
-  getValueAt: function(index) {
-    if (!this._results || index >= this._results.length) {
-      return null;
-    }
-    return this._results[index].value;
-  },
-
-  removeValueAt: function(index, removeFromDb) {
-    // FIXME: implement? why?
-  },
-
-  clearResults: function() {
+    this.searchResult = Ci.nsIAutoCompleteResult.RESULT_FAILURE;
     this._results = [];
-  },
+    this._listener.onSearchResult(this, this);
+  }
+}
 
-  addResult: function(aValue, aComment, aImage, aStyle) {
-    this._results.push({
-      value: aValue,
-      comment: aComment,
-      image: aImage,
-      style: aStyle
-    });
-  },
+// XPCOM registration
+SearchBase.prototype.QueryInterface = XPCOMUtils.generateQI(
+    [Ci.nsIAutoCompleteSearch, Ci.nsIAutoCompleteResult, Ci.ilISearchBase]);
 
-  notifyListener: function(aSucceeded) {
-    if (aSucceeded) {
-      this.matchCount = this._results.length;
-      this.searchResult = Ci.nsIAutoCompleteResult.RESULT_SUCCESS;
-      this._listener.onSearchResult(this, this);
-    } else {
-      this.matchCount = 0;
-      this.searchResult = Ci.nsIAutoCompleteResult.RESULT_FAILURE;
-      this._results = [];
-      this._listener.onSearchResult(this, this);
-    }
-  },
 
-  // XPCOM registration
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAutoCompleteSearch,
-      Ci.nsIAutoCompleteResult, Ci.ilISearchBase])
-};
-
+function NSGetModule(compMgr, fileSpec) {
+  return XPCOMUtils.generateModule([SearchBase]);
+}
