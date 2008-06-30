be939c20b430df225f0d2873368b5effe3f5df91
Ian McKellar <ian@mckellar.org>
2008-06-29 21:22:56 -0700

    remove useless dumps, persist section open state to prefs

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -3,3 +3,9 @@ pref('searchery.engines-enabled', true);
 pref('searchery.hide-searchbox', true);
 pref('searchery.engine.google', true);
 pref('searchery.engine.amazon', true);
+
+
+pref('extensions.searchery.section-enabled.places-section', true);
+pref('extensions.searchery.section-enabled.engines-section', true);
+pref('extensions.searchery.section-enabled.google-section', true);
+pref('extensions.searchery.section-enabled.amazon-section', true);
