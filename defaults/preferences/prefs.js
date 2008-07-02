9b27244880de030eb867bb4075bfd1311ff4fd3e
Ian McKellar <ian@mckellar.org>
2008-07-02 11:49:03 -0700

    disable amazon by default

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -4,4 +4,4 @@ pref('extensions.searchery.hide-searchbox', true);
 pref('extensions.searchery.section-enabled.places-section', true);
 pref('extensions.searchery.section-enabled.engines-section', true);
 pref('extensions.searchery.section-enabled.google-section', true);
-pref('extensions.searchery.section-enabled.amazon-section', true);
+pref('extensions.searchery.section-enabled.amazon-section', false);
